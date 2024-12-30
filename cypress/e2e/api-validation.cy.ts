3ba8f117267403cd25f42598b157117bea6a2e45
Alex von Hollen <56458322+Harrisgrade@users.noreply.github.com>
2024-12-30 17:42:38 -0600

    api-tests

--- cypress/e2e/api-validation.cy.ts
+++ cypress/e2e/api-validation.cy.ts
@@ -1,37 +1,11 @@
-import { createUserRequest} from "../fixtures/apiData";
+import { createUserRequest, createUserSchema } from "../fixtures/apiData";
 import addFormats from "ajv-formats";
 import AjvDraft04 from "ajv-draft-04";
 
 const ajv = new AjvDraft04();
 addFormats(ajv);
-
-const createUserSchema = 
-{
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "type": "object",
-  "properties": {
-    "code": {
-      "type": "integer"
-    },
-    "type": {
-      "type": "string"
-    },
-    "message": {
-      "type": "string"
-    }
-  },
-  "required": [
-    "code",
-    "type",
-    "message"
-  ]
-}
-
-
 describe("user operations about user", () => {
-
   it("create user and validate", () => {
-
     cy.request("POST", "https://petstore.swagger.io/v2/user/", {
       body: {
         createUserRequest,
@@ -39,15 +13,23 @@ describe("user operations about user", () => {
     }).then((response) => {
       expect(response.status).to.eq(200);
 
-      
       const data = response.body;
 
       const validate = ajv.compile(createUserSchema);
-        expect(validate(data)).to.be.true;
-
-    });
-      
-     
+      expect(validate(data)).to.be.true;
     });
   });
-
+  it("get user by username", () => {
+    cy.request("POST", "https://petstore.swagger.io/v2/user/", {
+      body: {
+        createUserRequest,
+      },
+    }).then((response) => {
+      expect(response.body).to.deep.equal({
+        code: 200,
+        message: "0",
+        type: "unknown",
+      });
+    });
+  });
+});
